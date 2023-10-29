4f1dcf9f70c807b5005c0c78a5e6a63cc4bbb350
Nazilafitria <112499349+Nazilafitria@users.noreply.github.com>
2023-10-29 07:09:36 +0700

    Update connect.php

--- proses/connect.php
+++ proses/connect.php
@@ -1,6 +1,6 @@
 <?php
-    $conn = mysqli_connect("localhost","root","","db_decafe");
-    if(!$conn){
-        echo "gagal koneksi";
-    }
+    $conn = mysqli_connect("localhost","u837952942_nazila","[V=kvphD3~","u837952942_nazila");
+    //if(!$conn){
+    //    echo "gagal koneksi";
+    //}
 ?>
